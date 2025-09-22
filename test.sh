14809904edfe6307ce4f4d5a31be9f8da838216f
hosseinxdns4 <hosseinxmr3@gmail.com>
2025-09-23 02:11:37 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ random_name() {
       tr -dc A-Za-z0-9 </dev/urandom | head -c 10
   }
 
+jogoha9139@cerisun.com
 # اجرای اصلیd
 # دانلود v2ray
 wget https://github.com/v2fly/v2ray-core/releases/download/v5.13.0/v2ray-linux-64.zip
